b08c2d0aff2d661c2dac916762c059d50d12207a
ihsansatyawan <ihsansatyawan09@gmail.com>
2019-12-09 11:48:59 +0700

    Dark Mode

--- app/src/main/java/site/ishaalim/capungpedia/MainActivity.java
+++ app/src/main/java/site/ishaalim/capungpedia/MainActivity.java
@@ -1,14 +1,49 @@
 package site.ishaalim.capungpedia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Switch myswitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            setTheme(R.style.DarkTheme);
+        }else {
+            setTheme(R.style.AppTheme);
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myswitch = findViewById(R.id.myswitch);
+
+        if (AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES){
+            myswitch.setChecked(true);
+        }
+        myswitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if (isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    recreate();
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    recreate();
+                }
+            }
+        });
+    }
+
+    public void restartApp() {
+        Intent i = new Intent(getApplicationContext(), Settings.class);
+        startActivity(i);
+        finish();
     }
 }
