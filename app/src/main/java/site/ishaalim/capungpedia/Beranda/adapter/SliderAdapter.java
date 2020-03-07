5c975c6db919da799d1092959182015fdbc5646b
ihsansatyawan <ihsansatyawan09@gmail.com>
2020-03-07 18:12:52 +0700

    Beranda : Slider Captions Html

--- app/src/main/java/site/ishaalim/capungpedia/Beranda/adapter/SliderAdapter.java
+++ app/src/main/java/site/ishaalim/capungpedia/Beranda/adapter/SliderAdapter.java
@@ -1,6 +1,7 @@
 package site.ishaalim.capungpedia.Beranda.adapter;
 
 import android.content.Context;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,26 +40,29 @@ public class SliderAdapter extends SliderViewAdapter<SliderViewHolder> {
 
     @Override
     public void onBindViewHolder(SliderViewHolder viewHolder, int position) {
+        String link1 = "https://firebasestorage.googleapis.com/v0/b/capung-pedia-cb932.appspot.com/o/Beranda_Header%201.jpg?alt=media&token=bc30d17d-5093-4c03-b829-d05e4b0d4493";
+        String link2 = "https://firebasestorage.googleapis.com/v0/b/capung-pedia-cb932.appspot.com/o/Beranda_Header%202.jpg?alt=media&token=4e2d03fe-34e5-489d-b4ee-f7e5aa1f59cc";
+        String link3 = "https://firebasestorage.googleapis.com/v0/b/capung-pedia-cb932.appspot.com/o/Beranda_Header%203.jpg?alt=media&token=fff9c7b9-d905-46d9-b6d4-34a92eacc003";
 
         switch(position){
             case 0:
-                viewHolder.tvHeader.setText("Capung-hutan pita-tunggal (Drepanosticta gazella) capung endemik Pulau Jawa");
+                viewHolder.tvHeader.setText(Html.fromHtml("Capung-hutan pita-tunggal (<i><b>Drepanosticta gazella</b></i>) capung endemik Pulau Jawa"));
                 Glide.with(viewHolder.itemView)
-                        .load("https://firebasestorage.googleapis.com/v0/b/capung-pedia-cb932.appspot.com/o/Beranda_Header%201.jpg?alt=media&token=bc30d17d-5093-4c03-b829-d05e4b0d4493")
+                        .load(link1)
                         .apply(options)
                         .into(viewHolder.imageViewBackGround);
                 break;
             case 1:
                 viewHolder.tvHeader.setText("Salah satu habitat capung di kawasan Desa Wisata Jatimulyo");
                 Glide.with(viewHolder.itemView)
-                        .load("https://firebasestorage.googleapis.com/v0/b/capung-pedia-cb932.appspot.com/o/Beranda_Header%202.jpg?alt=media&token=4e2d03fe-34e5-489d-b4ee-f7e5aa1f59cc")
+                        .load(link2)
                         .apply(options)
                         .into(viewHolder.imageViewBackGround);
                 break;
             case 2:
-                viewHolder.tvHeader.setText("Capung-jarum gelap (Prodasineura autumnalis) pada posisi kopulasi");
+                viewHolder.tvHeader.setText(Html.fromHtml("Capung-jarum gelap (<i><b>Prodasineura autumnalis</b></i>) pada posisi kopulasi"));
                 Glide.with(viewHolder.itemView)
-                        .load("https://firebasestorage.googleapis.com/v0/b/capung-pedia-cb932.appspot.com/o/Beranda_Header%203.jpg?alt=media&token=fff9c7b9-d905-46d9-b6d4-34a92eacc003")
+                        .load(link3)
                         .apply(options)
                         .into(viewHolder.imageViewBackGround);
                 break;
