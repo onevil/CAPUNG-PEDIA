719d65033ed8ca7d28b73babb441257a24105fe8
ihsansatyawan <ihsansatyawan09@gmail.com>
2020-03-10 12:16:06 +0700

    Notification List : OnClick itemView

--- app/src/main/java/site/ishaalim/capungpedia/notification/adapter/notificationAdapter.java
+++ app/src/main/java/site/ishaalim/capungpedia/notification/adapter/notificationAdapter.java
@@ -1,6 +1,7 @@
 package site.ishaalim.capungpedia.notification.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,7 +16,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.TimeZone;
 
+import maes.tech.intentanim.CustomIntent;
 import site.ishaalim.capungpedia.R;
+import site.ishaalim.capungpedia.notification.NotificationActivity;
 import site.ishaalim.capungpedia.notification.model.notification;
 import site.ishaalim.capungpedia.notification.viewHolder.notificationViewHolder;
 
@@ -23,7 +26,7 @@ public class notificationAdapter extends RecyclerView.Adapter<notificationViewHo
     private ArrayList<notification> notificationArrayList;
     Context context;
     Date date;
-    String body, notifDate;
+    String body, URL;
 
     public notificationAdapter(ArrayList<notification> notificationArrayList, Context context) {
         this.notificationArrayList = notificationArrayList;
@@ -43,15 +46,31 @@ public class notificationAdapter extends RecyclerView.Adapter<notificationViewHo
     public void onBindViewHolder(@NonNull notificationViewHolder holder, int position) {
         body = notificationArrayList.get(position).getMessageBody();
         date = notificationArrayList.get(position).getNotifDate();
+        URL = notificationArrayList.get(position).getNotifUrl();
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd MMMM yyyy");
         dateFormat.setTimeZone(TimeZone.getTimeZone("GMT+7"));
 
         holder.tvBody.setText(Html.fromHtml(body));
         holder.tvDate.setText(dateFormat.format(date));
+
+        setEvents(holder.itemView, URL);
     }
 
     @Override
     public int getItemCount() {
         return notificationArrayList.size();
     }
+
+    private void setEvents(View itemView, final String url) {
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, NotificationActivity.class);
+                intent.putExtra("URL",url);
+                context.startActivity(intent);
+                CustomIntent.customType(context, "left-to-right");
+            }
+        });
+    }
 }
+
